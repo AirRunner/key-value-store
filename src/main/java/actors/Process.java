1bec098f5173e3e15f691cde711ec38b692408df
AirRunner <luca.vaio@efrei.net>
2020-11-04 13:40:31 +0100

    Added seqNumber check and split onReceive()

--- src/main/java/actors/Process.java
+++ src/main/java/actors/Process.java
@@ -3,6 +3,8 @@ package actors;
 import akka.actor.ActorRef;
 import akka.actor.Props;
 import akka.actor.UntypedAbstractActor;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -28,6 +30,7 @@ public class Process extends UntypedAbstractActor {
 	private Thread restTimer;
 	private long chrono;
 	private int proposal;
+	private ArrayList<UUID> currSeqNumbers;
 	private int ackNumber;
 
 	public Process(int ID, int nb) {
@@ -40,6 +43,7 @@ public class Process extends UntypedAbstractActor {
 		this.timestamps = new HashMap<>();
 		this.state = State.NONE;
 		this.restTimer = new Thread(new RestTimer(this));
+		this.currSeqNumbers = new ArrayList<>();
 		this.ackNumber = 0;
 	}
 
@@ -70,87 +74,27 @@ public class Process extends UntypedAbstractActor {
 			}
 			/** Process GET operation **/
 			else if (message instanceof Get) {
-				Get get = (Get) message;
-				this.chrono = System.nanoTime();
-				this.state = State.GET;
-				this.log.info("p" + self().path().name() + " is launching a get request with key " + get.key + "...");
-				sendRequests(Request.READ, get.key);
-				// this.log.info("p" + self().path().name() + " launched a get request with key '" + get.key);
+				getReceived((Get) message);
 			}
 			/** Process PUT operation **/
 			else if (message instanceof Put) {
-				Put put = (Put) message;
-				this.chrono = System.nanoTime();
-				this.state = State.PUT;
-				this.proposal = put.proposal;
-				this.log.info("p" + self().path().name() + " is launching a put request with key " + put.key + " and proposal " + put.proposal + "...");
-				sendRequests(Request.READ, put.key);
-				// this.log.info("p" + self().path().name() + " launched a put request with key " + put.key + " and proposal " + put.proposal);
+				putReceived((Put) message);
 			}
 			/** Process read request **/
 			else if (message instanceof ReadRequest) {
-				ReadRequest rq = (ReadRequest) message;
-				int readValue = this.values.containsKey(rq.key) ? this.values.get(rq.key) : 0;
-				int readTimestamp = this.timestamps.containsKey(rq.key) ? this.timestamps.get(rq.key) : 0;
-				ReadResponse rs = new ReadResponse(rq.seqNumber, rq.key, readValue, readTimestamp);
-				sender().tell(rs, self());
-				// this.log.info("p" + self().path().name() + " responded to a read request from p" + sender().path().name());
+				readReqReceived((ReadRequest) message);
 			}
 			/** Process read response **/
 			else if (message instanceof ReadResponse && (this.state == State.GET || this.state == State.PUT)) {
-				ReadResponse rs = (ReadResponse) message;
-				this.ackNumber++;
-				int readTimestamp = this.timestamps.containsKey(rs.key) ? this.timestamps.get(rs.key) : 0;
-				int readValue = this.values.containsKey(rs.key) ? this.values.get(rs.key) : 0;
-				if (rs.timestamp > readTimestamp || (rs.timestamp == readTimestamp && rs.value > readValue)) {
-					this.timestamps.put(rs.key, rs.timestamp);
-					this.values.put(rs.key, rs.value);
-				}
-				/** Majority **/
-				if (this.ackNumber >= this.N/2) {
-					this.ackNumber = 0;
-					if (this.state == State.GET) {
-						this.state = State.NONE;
-						this.chrono = System.nanoTime() - this.chrono;
-						this.log.info("p" + self().path().name() + " got the value [" + this.values.get(rs.key) + "] with key [" +
-							rs.key + "] and timestamp [" + this.timestamps.get(rs.key) + "] in " + this.chrono / 1000 + "μs");
-						nextOperation();
-					}
-					else if (this.state == State.PUT) {
-						readTimestamp = this.timestamps.containsKey(rs.key) ? this.timestamps.get(rs.key) : 0;
-						this.timestamps.put(rs.key, readTimestamp + 1);
-						this.values.put(rs.key, this.proposal);
-						this.state = State.WAIT_WRITE;
-						sendRequests(Request.WRITE, rs.key);
-					}
-				}
+				readRespReceived((ReadResponse) message);
 			}
 			/** Process write request **/
 			else if (message instanceof WriteRequest) {
-				WriteRequest wq = (WriteRequest) message;
-				int putTimestamp = this.timestamps.containsKey(wq.key) ? this.timestamps.get(wq.key) : 0;
-
-				if (wq.timestamp > putTimestamp || (wq.timestamp == putTimestamp && wq.proposal > this.values.get(wq.key))) {
-					this.timestamps.put(wq.key, wq.timestamp);
-					this.values.put(wq.key, wq.proposal);
-				}
-				WriteResponse ws = new WriteResponse(wq.seqNumber, wq.key);
-				sender().tell(ws, self());
-				// this.log.info("p" + self().path().name() + " responded to a write request from p" + sender().path().name());
+				writeReqReceived((WriteRequest) message);
 			}
 			/** Process write response **/
 			else if (message instanceof WriteResponse && this.state == State.WAIT_WRITE) {
-				WriteResponse wr = (WriteResponse) message;
-				this.ackNumber++;
-				/** Majority **/
-				if (this.ackNumber >= this.N/2) {
-					this.ackNumber = 0;
-					this.state = State.NONE;
-					this.chrono = System.nanoTime() - this.chrono;
-					this.log.info("p" + self().path().name() + " put the value [" + this.proposal + "] with key [" + wr.key +
-						"] and timestamp [" + this.timestamps.get(wr.key) + "] in " + this.chrono / 1000 + "μs");
-					nextOperation();
-				}
+				writeRespReceived((WriteResponse) message);
 			}
 		}
 		/** The process is faulty **/
@@ -182,6 +126,88 @@ public class Process extends UntypedAbstractActor {
 
 
 	/** Private methods **/
+	private void getReceived(Get msg) throws Throwable {
+		this.chrono = System.nanoTime();
+		this.state = State.GET;
+		this.log.info("p" + self().path().name() + " is launching a get request with key " + msg.key + "...");
+		sendRequests(Request.READ, msg.key);
+		// this.log.info("p" + self().path().name() + " launched a get request with key '" + get.key);
+	}
+
+	private void putReceived(Put msg) throws Throwable {
+		this.chrono = System.nanoTime();
+		this.state = State.PUT;
+		this.proposal = msg.proposal;
+		this.log.info("p" + self().path().name() + " is launching a put request with key " + msg.key + " and proposal " + msg.proposal + "...");
+		sendRequests(Request.READ, msg.key);
+		// this.log.info("p" + self().path().name() + " launched a put request with key " + message.key + " and proposal " + message.proposal);
+	}
+
+	private void readReqReceived(ReadRequest msg) {
+		int readValue = this.values.containsKey(msg.key) ? this.values.get(msg.key) : 0;
+		int readTimestamp = this.timestamps.containsKey(msg.key) ? this.timestamps.get(msg.key) : 0;
+		ReadResponse rs = new ReadResponse(msg.seqNumber, msg.key, readValue, readTimestamp);
+		sender().tell(rs, self());
+		// this.log.info("p" + self().path().name() + " responded to a read request from p" + sender().path().name());
+	}
+	
+	private void readRespReceived(ReadResponse msg) throws Throwable {
+		if (this.currSeqNumbers.contains(msg.seqNumber)) {
+			this.ackNumber++;
+			int readTimestamp = this.timestamps.containsKey(msg.key) ? this.timestamps.get(msg.key) : 0;
+			int readValue = this.values.containsKey(msg.key) ? this.values.get(msg.key) : 0;
+			if (msg.timestamp > readTimestamp || (msg.timestamp == readTimestamp && msg.value > readValue)) {
+				this.timestamps.put(msg.key, msg.timestamp);
+				this.values.put(msg.key, msg.value);
+			}
+			/** Majority **/
+			if (this.ackNumber >= this.N/2) {
+				this.ackNumber = 0;
+				if (this.state == State.GET) {
+					this.state = State.NONE;
+					this.chrono = System.nanoTime() - this.chrono;
+					this.log.info("p" + self().path().name() + " got the value [" + this.values.get(msg.key) + "] with key [" +
+						msg.key + "] and timestamp [" + this.timestamps.get(msg.key) + "] in " + this.chrono / 1000 + "μs");
+					nextOperation();
+				}
+				else if (this.state == State.PUT) {
+					readTimestamp = this.timestamps.containsKey(msg.key) ? this.timestamps.get(msg.key) : 0;
+					this.timestamps.put(msg.key, readTimestamp + 1);
+					this.values.put(msg.key, this.proposal);
+					this.state = State.WAIT_WRITE;
+					sendRequests(Request.WRITE, msg.key);
+				}
+			}
+		}
+	}
+
+	private void writeReqReceived(WriteRequest msg) {
+		int putTimestamp = this.timestamps.containsKey(msg.key) ? this.timestamps.get(msg.key) : 0;
+
+		if (msg.timestamp > putTimestamp || (msg.timestamp == putTimestamp && msg.proposal > this.values.get(msg.key))) {
+			this.timestamps.put(msg.key, msg.timestamp);
+			this.values.put(msg.key, msg.proposal);
+		}
+		WriteResponse ws = new WriteResponse(msg.seqNumber, msg.key);
+		sender().tell(ws, self());
+		// this.log.info("p" + self().path().name() + " responded to a write request from p" + sender().path().name());
+	}
+
+	private void writeRespReceived(WriteResponse msg) throws Throwable {
+		if (this.currSeqNumbers.contains(msg.seqNumber)) {
+			this.ackNumber++;
+			/** Majority **/
+			if (this.ackNumber >= this.N/2) {
+				this.ackNumber = 0;
+				this.state = State.NONE;
+				this.chrono = System.nanoTime() - this.chrono;
+				this.log.info("p" + self().path().name() + " put the value [" + this.proposal + "] with key [" + msg.key +
+					"] and timestamp [" + this.timestamps.get(msg.key) + "] in " + this.chrono / 1000 + "μs");
+				nextOperation();
+			}
+		}
+	}
+
 	private void nextOperation() throws Throwable {
 		if (!this.mailbox.isEmpty()) {
 			this.onReceive(this.mailbox.remove());
@@ -195,13 +221,17 @@ public class Process extends UntypedAbstractActor {
 	private void sendRequests(Request type, int key) throws InterruptedException {
 		Thread.sleep(1);
 		Object request;
+		UUID newSeqNumber;
+		this.currSeqNumbers.clear();
 		for (ActorRef actor : this.processes.references) {
 			if (actor != self()) {
+				newSeqNumber = UUID.randomUUID();
+				this.currSeqNumbers.add(newSeqNumber);
 				if (type == Request.WRITE) {
-					request = (WriteRequest) new WriteRequest(UUID.randomUUID(), key, this.values.get(key), this.timestamps.get(key));
+					request = (WriteRequest) new WriteRequest(newSeqNumber, key, this.values.get(key), this.timestamps.get(key));
 				}
 				else {
-					request = (ReadRequest) new ReadRequest(UUID.randomUUID(), key);
+					request = (ReadRequest) new ReadRequest(newSeqNumber, key);
 				}
 				actor.tell(request, self());
 			}
